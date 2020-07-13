d42768ddb4a5ca72b706711fbf56a406ac156537
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-13 09:43:57 -0400

    build out initial all lists view

--- README.md
+++
@@ -1,3 +0,0 @@
-# About
-
-This is a larger ToDo list project built using the Sinatra web framework as a part of [Launch School's](https://launchschool.com) course **LS175 - Networked Applications**.
--- todo.rb
+++ todo.rb
@@ -3,5 +3,12 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists" # so home page "/" will just take user to the "/lists" listing, what we want, https://launchschool.com/lessons/9230c94c/assignments/7bdd9818
+end
+
+get "/lists" do
+  @lists = [{name: "Lunch Groceries", todos: []},  # each individual list will be a hash with a name key-value pair hash, and nested items hash with key value pairs
+            {name: "Dinner Groceries", todos: []}
+          ]
+  erb :lists, layout: :layout # added a lists file, https://launchschool.com/lessons/9230c94c/assignments/7bdd9818
 end
