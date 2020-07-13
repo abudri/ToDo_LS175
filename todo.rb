c4bb9e9ef1b2f27d63a58932c2785907ebc2b53b
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-13 10:50:57 -0400

    initial hardcoded session data creation, and new list link

--- todo.rb
+++ todo.rb
@@ -2,13 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions # tells sinatra to activate it's session support
+  set :sessions_secret, 'secret' # setting the session secret, to the string 'secret'
+end
+
+before do
+  session[:lists] ||= [] # recall that  `session` is a hash, and for key :lists, the value is an array of lists, and each list itself is a hash  # to ensure we at least have an empty array if session[:lists] is non-existent, or nil, https://launchschool.com/lessons/9230c94c/assignments/2f3d171a
+end
+
 get "/" do
   redirect "/lists" # so home page "/" will just take user to the "/lists" listing, what we want, https://launchschool.com/lessons/9230c94c/assignments/7bdd9818
 end
 
 get "/lists" do
-  @lists = [{name: "Lunch Groceries", todos: []},  # each individual list will be a hash with a name key-value pair hash, and nested items hash with key value pairs
-            {name: "Dinner Groceries", todos: []}
-          ]
+  @lists = session[:lists] # pull lists from session data
   erb :lists, layout: :layout # added a lists file, https://launchschool.com/lessons/9230c94c/assignments/7bdd9818
+end
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: []} # a list is a hash that has two values, a name that is a string, and a todos key that is an array of todos / list items
+  redirect "/lists"
 end
