bb5a9ca9b5b3af92827a5cb66ba9e652c88df248
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-14 12:49:50 -0400

    add view for single list, and also moved some link content to layouts file

--- todo.rb
+++ todo.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
@@ -28,8 +29,8 @@ get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
-# returns an error messagse if the name of the list attempted to be submitted is not valid and if so returns a string, otherwise will return nil
-def error_for_list_name(name) # for refactoring validations assignment, https://launchschool.com/lessons/9230c94c/assignments/b47401cd
+# returns an error messagse if the name of the list attempted to be submitted is not valid and if so returns a string, otherwise will return nil. This is for refactoring validations assignment, https://launchschool.com/lessons/9230c94c/assignments/b47401cd
+def error_for_list_name(name) 
   if !(1..100).cover?(name.size) # if the list_name is NOT between 1 and 100 characters, instead of using >= and <= operators, see:
     'The list name must be between 1 and 100 characters.' # https://launchschool.com/lessons/9230c94c/assignments/7923bc3a, refactored into this new method at: https://launchschool.com/lessons/9230c94c/assignments/b47401cd
   elsif session[:lists].any? { |list| list[:name] == name } # iterates through all lists in the session and for each checks if the :name is equal to name the user tried to submit in the form
@@ -41,7 +42,8 @@ end
 post '/lists' do
   list_name = params[:list_name].strip # for use in checking if name passed in as a param is valid(exists, not too long or short) before saving, see: https://launchschool.com/lessons/9230c94c/assignments/7923bc3a // .strip to remove any leading or trailing whitespace
 
-  if error = error_for_list_name(list_name) # method call returns a string error message from the method if the list_name passed in is invalid, otherwise it will return nil and this branch of the if statement won't be executed. assign and evaluate at the same time. everything in Ruby is an expression, so the assingment here returns a value just as any other expression, so can test value while at the same time assigning.
+  error = error_for_list_name(list_name) # method call returns a string error message from the method if the list_name passed in is invalid, otherwise it will return nil and the first branch of the if statement won't be executed.
+  if error
     session[:error] = error # refactored at: https://launchschool.com/lessons/9230c94c/assignments/b47401cd
     erb :new_list, layout: :layout
   else # create the new list name since the above two validations passed
@@ -49,4 +51,11 @@ post '/lists' do
     session[:success] = 'The list has been created.' # flash message for successful list creation https://launchschool.com/lessons/9230c94c/assignments/cfb2f0cb
     redirect '/lists'
   end
+end
+
+# view an individual list
+get '/lists/:id' do # id in the URL is a parameter that we will be using in this method
+  id = params[:id].to_i # converting the "1" in "/lists/1" into an integer to get the list based off index from the hash session[:lists], which returns and is an array
+  @list = session[:lists][id]
+  erb :list, layout: :layout
 end
