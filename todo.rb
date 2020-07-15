1f48611877e956f4f1080efb7c8a38e127feb536
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-15 13:46:02 -0400

    added ability to create a todo list item, no validations quite yet

--- todo.rb
+++ todo.rb
@@ -5,6 +5,13 @@ require 'sinatra/reloader'
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
+# session - hash // it's key :lists - is an array - see `session[:lists] ||= []`. it also has keys :error, :success for flash messages
+# session[:lists] << { name: list_name, todos: [] }
+
+# list - hash with key :name a value of string, and key :todos which is an array. see `session[:lists] << { name: list_name, todos: [] } `
+
+# todo - each todo item in a list is a hash itself, contained in the array value of list[:todos], `list[:todos] << { name: params[:todo], completed: false }`
+
 configure do
   enable :sessions # tells sinatra to activate it's session support
   set :sessions_secret, 'secret' # setting the session secret, to the string 'secret'
@@ -30,7 +37,7 @@ get '/lists/new' do
 end
 
 # returns an error messagse if the name of the list attempted to be submitted is not valid and if so returns a string, otherwise will return nil. This is for refactoring validations assignment, https://launchschool.com/lessons/9230c94c/assignments/b47401cd
-def error_for_list_name(name) 
+def error_for_list_name(name)
   if !(1..100).cover?(name.size) # if the list_name is NOT between 1 and 100 characters, instead of using >= and <= operators, see:
     'The list name must be between 1 and 100 characters.' # https://launchschool.com/lessons/9230c94c/assignments/7923bc3a, refactored into this new method at: https://launchschool.com/lessons/9230c94c/assignments/b47401cd
   elsif session[:lists].any? { |list| list[:name] == name } # iterates through all lists in the session and for each checks if the :name is equal to name the user tried to submit in the form
@@ -70,7 +77,7 @@ end
 # updates an existing todo list, handles saving from edit_list.erb. much of code is taken from post '/lists' do route
 post '/lists/:id' do
   list_name = params[:list_name].strip # for use in checking if name passed in as a param is valid(exists, not too long or short) before saving, see: https://launchschool.com/lessons/9230c94c/assignments/7923bc3a // .strip to remove any leading or trailing whitespace
-  id = params[:id].to_i  # from edit existing list method above
+  id = params[:id].to_i # from edit existing list method above
   @list = session[:lists][id] # from edit existing list method above
 
   error = error_for_list_name(list_name) # method call returns a string error message from the method if the list_name passed in is invalid, otherwise it will return nil and the first branch of the if statement won't be executed.
@@ -86,8 +93,17 @@ end
 
 # delete an individual list, https://launchschool.com/lessons/9230c94c/assignments/ace30260
 post '/lists/:id/destroy' do
-  id = params[:id].to_i  # from edit existing list method above
+  id = params[:id].to_i # from edit existing list method above
   session[:lists].delete_at(id) # remove the list - which is a hash itself, from the session array - using .delete_at, which will delete at the specified index you pass to it, in our case the id is our index
-  session[:success] = 'The list has been deleted.' 
+  session[:success] = 'The list has been deleted.'
   redirect '/lists' # redirect to the home page which is '/lists'
+end
+
+# add a todo item to an individual list: https://launchschool.com/lessons/9230c94c/assignments/046ee3e0
+post "/lists/:list_id/todos" do
+  list_id = params[:list_id].to_i # from edit existing list method above, id of the list, but since using todo items, we say :list_id
+  list = session[:lists][list_id]
+  list[:todos] << { name: params[:todo], completed: false } # params[:todo] is the submitted text taken from form submission at the list.erb page submit form for a todo item, which is named "todo"
+  session[:success] = 'The todo item was added to the list'
+  redirect "/lists/#{list_id}" # redirect back to the list we just added the item to
 end
