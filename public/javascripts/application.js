b585dd886d34ee4c522f6f9dd9861bfa3017b8cf
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-23 16:54:33 -0400

    implementing some AJAX into project

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,22 @@
 $(function () {
-  $("form.delete").submit(function (event) {
+  $("form.delete").submit(function(event) {
     event.preventDefault();
     event.stopPropagation();
 
     var ok = confirm("Are you sure? This cannot be undone.");
     if (ok) {
       this.submit();
+
+      var form = $(this); // from assignment: https://launchschool.com/lessons/2c69904e/assignments/94ee8ca2
+
+      var request = $.ajax({
+        url: form.attr("action"),
+        method: form.attr("method")
+      });
+
+      request.done(function(data, textStatus, jqXHR) {
+        form.parent("li").remove()
+      });
     }
-    
   });
 });
